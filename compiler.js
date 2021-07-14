f16d1018ff3fd462538f5bd8e2735b23fa4dc233
Kevin Souza <igantekevin@hotmail.com>
2021-07-14 09:23:14 -0300

    Utilização do inpostfix

--- compiler.js
+++ compiler.js
@@ -1,22 +1,3 @@
-// https://medium.com/@henriquesosa/seja-um-compilador-crie-um-compilador-com-javascript-33a9557c8cdf
-
-/* Passo por todas as letras formando uma array com o informação desse lexema */
-function klexer(code) {
-    return code.split('')
-        .filter(function (t) { return t.length > 0 })
-        .map((t) => {
-            if (isLetter(t))
-                return { type: 'variable', value: t }
-            else
-                return { type: 'simbol', value: t }
-
-        })
-}
-
-function isLetter(str) {
-    return str.length === 1 && str.search(/[a-z]/) >= 0;
-}
-
 /* 
    Remove os espacos em branco
 */
@@ -29,67 +10,204 @@ function sanitize(string) {
 */
 function replace_tokens(string) {
     tmp = string;
-    tmp = tmp.replaceAll('<->', 'B');
-    tmp = tmp.replaceAll('->', 'C');
-    tmp = tmp.replaceAll('^', 'A'); // A de and, estava bugando
+    tmp = tmp.replaceAll('<->', '+');
+    tmp = tmp.replaceAll('->', '-');
+    tmp = tmp.replaceAll('V', '/');
+    //tmp = tmp.replaceAll('^', 'A'); // A de and, estava bugando
     return tmp;
 }
 
-/* funcoes mortas */
-/* -> C */
-function se(a, b) {
-    return (a == true && b == false) ? 0 : 1;
-}
 
-/* <-> B */
-function bicond(a, b) {
-    return (a != b) ? 0 : 1;
-}
+'use strict';
+/* Fonte:  https://www.npmjs.com/package/inpostfix*/
+/**
+ * Given a string, returns a number if you can. Else return what was given.
+ * @param {*} s
+ * @returns {number|*}
+ */
+const maybeNumber = s => {
+  if (s === null) {
+    return s;
+  }
+  const p = 1 * s;
+  return Number.isNaN(p) ? s : p;
+};
+
+const NEG = '~';
+
+const OPS = new Map()
+    .set(NEG, [5, 'L', 1, a => !a]) // A unary negative symbol
+    //.set('^', [4, 'R', 2, (b, a) => Math.pow(a, b)])
+    .set('^', [3, 'L', 2, (b, a) =>  a && b])
+    .set('/', [3, 'L', 2, (b, a) => a || b]) // V
+    //.set('%', [3, 'L', 2, (b, a) => a % b])
+    .set('+', [2, 'L', 2, (b, a) => (a != b) ? 0 : 1]) //B
+    .set('-', [2, 'L', 2, (b, a) => (a == true && b == false) ? 0 : 1]); //C
 
 
-class Node {
-    constructor(value = 0, left = 0, right = 0) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-        this.type = 0
-        this.preVal = value;
-    }
-}
 
-/* Vai gerando o arvore de operações  */
-function buildAst(node, caracter)
-{
-    if (node.value === 0)
-        return
+const ops = ['(', ')', '^', '-', '+', '/', NEG];
 
-    //somente procura se for 0 ou variable
-    if (node.type === 'variable' || node.type === 0)
-    {
-        let i = node.value.search(caracter);
+const unary = [];
 
-        if (i >= 0) {
-            node.type = 'operation';
+const brackets = ['(', ')'];
 
-            let ar = [node.value.substring(0, i), node.value.substring(i + 1,  node.value.length)]
-            node.left = new Node(ar[0]);
-            node.right = new Node(ar[1]);
-            node.value = caracter;
-            buildAst(node.left,caracter);
-            buildAst(node.right,caracter);
+const isOpp = e => ops.includes(e);
+
+const isUnary = e => unary.includes(e);
+
+const isNegate = e => e === unary[0];
+
+const isBrackets = e => brackets.includes(e);
+
+const isLeftBracket = e => e === brackets[0];
+
+const noEmptyStrings = e => e !== '';
+
+const noCommas = e => e !== ',';
+
+const isUnarySymbol = (lc, i) =>
+    lc === undefined || i === 0 || isOperator(lc) || isLeftBracket(lc);
+
+const letterMatch = /[a-z]/i;
+const digitMatch = /[0-9]/;
+
+const isLetter = e => !!e.match(letterMatch);
+
+const isDigit = e => !!e.match(digitMatch);
+
+const isDecimalPoint = e => e === '.';
+
+const isNumString = e => isLetter(e) || isDigit(e) || isDecimalPoint(e);
+
+const isNumber = n => typeof n === 'number' && !Number.isNaN(n);
+
+const isOperator = e => OPS.has(e);
+
+const isLeftAss = e => OPS.get(e)[1] === 'L';
+
+const isFunction = (e, ne) => typeof(e) === 'string' && typeof(ne) === 'string' && isLetter(e) && isLeftBracket(ne);
+
+const peek = a => a[a.length - 1];
+
+const hasEls = a => a.length > 0;
+
+const opPres = e => isOperator(e) ? OPS.get(e)[0] : 0;
+
+const t1 = (o1, o2) => isLeftAss(o1) && opPres(o1) <= opPres(o2);
+
+const t2 = (o1, o2) => opPres(o1) < opPres(o2);
+
+const tokenize = infixString => infixString
+    .split('')
+    .reduce(
+        ([p, n], e, i) => {
+          n = isNumString(e)
+              ? n.push(e) && n
+              : (hasEls(n)
+                  ? p.push(n.join('')) && []
+                  : n);
+
+          p = isOpp(e)
+              ? p.push(e) && p
+              : (isUnary(e)
+                  ? (isUnarySymbol(peek(p), i)
+                          ? isNegate(e) ? p.push(NEG) && p : p
+                          : p.push(e) && p)
+                  : p);
+
+          return [p, n];
+        }, [[], []])
+    .reduce((p, c, i) => i ? [...p, c.join('')] : c, [])
+    .filter(noEmptyStrings)
+    .filter(noCommas)
+    .map(maybeNumber);
+
+
+const toRPN = infixString => tokenize(infixString)
+    .reduce(([rS, oS, fnArgs], c, i, t) => {
+      if (isFunction(c, t[i+1])) {
+        oS.push(c + "()");
+        fnArgs.push(0);
+      }
+      else if (isBrackets(c)) {
+        if (isLeftBracket(c)) {
+          oS.push(c);
         }
-        else
-        {
-            node.type = 'variable' ;
-            return 0;
+        else {
+          let nextOpp = oS.pop();
+          while (nextOpp !== '(') {
+            rS.push(nextOpp);
+            if (fnArgs.length > 0) fnArgs[fnArgs.length-1]++;
+            nextOpp = oS.pop();
+          }
+          if (fnArgs.length > 0 && oS[oS.length-1].endsWith('()')) {
+            oS[oS.length-1] = oS[oS.length-1].replace("()",`(${fnArgs[fnArgs.length-1]})`);
+            fnArgs.pop();
+          }
         }
-    }
-    else
-    {
-        buildAst(node.left,caracter);
-        buildAst(node.right,caracter);
-    }
+      } else if (!isOperator(c)) {
+        rS.push(c);
+        if (fnArgs.length > 0) fnArgs[fnArgs.length-1]++;
+      } else {
+        const o1 = c;
+        let o2 = peek(oS);
+        let c1 = t1(o1, o2);
+        let c2 = t2(o1, o2);
 
-    
-    
-}
+        while (o2 && (c1 || c2)) {
+          rS.push(oS.pop());
+          o2 = peek(oS);
+          c1 = t1(o1, o2);
+          c2 = t2(o1, o2);
+        }
+        oS.push(o1);
+      }
+      return [rS, oS, fnArgs];
+    }, [[], [], []])
+    .reduce((p, c, i) => i === 1 ? [...p, ...c.reverse()] : i === 0 ? c : p, []);
+ 
+const lookupVar = (v, lookup) => {
+  if (typeof(v) !== 'string') return v;
+  if (lookup[v] !== undefined) return lookup[v];
+  return v;
+};
+
+const calcRPN = (rpn, lookup) => rpn.reduce((p, c) => {
+  if (isNumber(c)) {
+    p.push(c);
+    return p;
+  }
+  let isFn = c.match(/(.*)\((\d+)\)/);
+  if (!isFn && isLetter(c)) {
+    p.push(lookupVar(c, lookup));
+    return p;
+  }
+  let arity = 0, opp = null;
+  if (isFn) {
+    arity = parseInt(isFn[2], 10);
+    opp = lookupVar(isFn[1], lookup);
+  }
+  else {
+    arity = OPS.get(c)[2];
+    opp = OPS.get(c)[3];
+  }
+  const args = [];
+  if (arity > 1) {
+    for (let i=0; i<arity; i++) {
+      args.push(lookupVar(p.pop(), lookup));
+    }
+  }
+  p.push(arity === 1
+      ? opp(lookupVar(p.pop(), lookup))
+      : opp(...args));
+  return p;
+}, [])[0];
+
+const calcInfix = (s, lookup) => calcRPN(toRPN(s), lookup);
+
+
+
+/* Função gerador de tabela verdade */
+
+/* Função que evalua uma tabela verdade junto com a expressão */
--- index.js
+++ index.js
@@ -5,52 +5,19 @@ function compile(string)
     console.log(lex); */
 }
 
-
-// let a = new Node("pCq^pV~qCp^q")
-// let a = new Node(0)
-
-/* let a = new Node('pCqCp') // funciona como o esperado
-findCharacterAndSplice(a,'C') */
-
-/* let a = new Node('pVqVp') // funciona como o esperado
-findCharacterAndSplice(a,'V') */
+//let a = '(p->q)^~(qV(~q->(q^p)^q))';
+let a = '(p->q)';
 
 
-/* let a = new Node('pCqVp') 
-findCharacterAndSplice(a,'C') 
-findCharacterAndSplice(a,'V') // perfeito até aqui */
+a = sanitize(a);
+a = replace_tokens(a);
+//troco as variáveis
+let b = a.replaceAll('p',1);
+b = b.replaceAll('q',0);
 
+/* console.log(b);
+console.log(tokenize(a));
+console.log(toRPN(a));
+console.log(calcRPN(toRPN(b))); */
 
-//let a = new Node('pCqVqCp') 
-// qVpCp
-/* let a = new Node('pCqVqCp') 
-buildAst(a,'C')
-buildAst(a,'V')
-// buildAst(a,'V') // perfeito até aqui  */
-
-
-
-
-/*  //Ele não exerga ^, Vou trocar por A de And
- let a = new Node('pCqVq^~p') 
-buildAst(a,'C')
-buildAst(a,'V')
-buildAst(a,'^') 
-let a = new Node('qA~p') 
-buildAst(a,'A') */
-
-
-/* let a = new Node('qA~p') 
-buildAst(a,'A') 
-buildAst(a,'~')  */
-
-
-//Ele não exerga ^, Vou trocar por A de And
-let a = new Node(replace_tokens( sanitize('qVq^~pCp')) ) 
-buildAst(a,'C')
-buildAst(a,'V')
-buildAst(a,'A')
-buildAst(a,'~')
-
-// olhe no console e siga a arvore, está tudo ok
-console.log(a);
+console.log(calcInfix(b));
